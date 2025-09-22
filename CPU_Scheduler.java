96b5d751a9dc50c4f91b2ea351014e2fceba1eaa
Aayoush Chowdhury <aayoushchowdhury@Aayoushs-Laptop.local>
2025-09-22 13:27:22 -0400

    Added CPU Scheduler to extracr the data from process.txt

---
+++ CPU_Scheduler.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class CPU_Scheduler 
+{
+    public static void main(String[] args) 
+    {
+        List<process> processes = readProcessesFromFile("processes.txt");
+        
+    }
+    public static List<process> readProcessesFromFile(String filename)
+    {
+
+    }
+    
+
+    
+}

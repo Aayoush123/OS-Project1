9fd44afb5a44a426b9db5e1382e1e3eac8d00a62
Aayoush Chowdhury <aayoushchowdhury@Aayoushs-Laptop.local>
2025-09-22 13:01:34 -0400

    Added variables and constructor to process.java

--- process.java
+++ process.java
@@ -1,3 +1,15 @@
-public class process {
-    
+public class process 
+{
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int priority; 
+
+    public process(int pid, int arrival_time, int burst_time, int priority) 
+    {
+        this.pid = pid;
+        this.arrival_time = arrival_time;
+        this.burst_time = burst_time;
+        this.priority = priority;
+    }
 }
